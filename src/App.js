49bd312fca5b6316ad685571314649df67d479c6
Marius Polverea <polvereamarius@yahoo.com>
2022-03-01 20:14:13 +0200

    React-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
 
 import './App.css'
+
+const HatsPage = () => {
+  return <>
+    <h1>Hats Page</h1>
+  </>
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path ='/' component={HomePage} />
+        <Route exact path ='/shop/hats' component={HatsPage} />
+      </Switch>
+
     </div>
   )
 }
